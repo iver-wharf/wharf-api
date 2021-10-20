ae898cac3be52e36eed9047c7a8575e81c16f6ad
Kalle Jillheden <kalle.jillheden@iver.se>
2021-10-20 14:01:16 +0200

    Renamed lhs->left, rhs->right

--- internal/set/string_test.go
+++ internal/set/string_test.go
@@ -8,140 +8,142 @@ import (
 
 type testCaseString struct {
 	name string
-	lhs  String
-	rhs  String
-	want String
+	// left hand side of the set operator
+	left String
+	// right hand side of the set operator
+	right String
+	want  String
 }
 
 func TestStringDifference(t *testing.T) {
 	testCases := []testCaseString{
 		{
-			name: "both empty",
-			lhs:  NewString(),
-			rhs:  NewString(),
-			want: NewString(),
+			name:  "both empty",
+			left:  NewString(),
+			right: NewString(),
+			want:  NewString(),
 		},
 		{
-			name: "empty rhs",
-			lhs:  NewString("a", "b", "c"),
-			rhs:  NewString(),
-			want: NewString("a", "b", "c"),
+			name:  "empty right",
+			left:  NewString("a", "b", "c"),
+			right: NewString(),
+			want:  NewString("a", "b", "c"),
 		},
 		{
-			name: "empty lhs",
-			lhs:  NewString(),
-			rhs:  NewString("a", "b", "c"),
-			want: NewString(),
+			name:  "empty left",
+			left:  NewString(),
+			right: NewString("a", "b", "c"),
+			want:  NewString(),
 		},
 		{
-			name: "ignores missing",
-			lhs:  NewString("a", "b", "c"),
-			rhs:  NewString("d", "e"),
-			want: NewString("a", "b", "c"),
+			name:  "ignores missing",
+			left:  NewString("a", "b", "c"),
+			right: NewString("d", "e"),
+			want:  NewString("a", "b", "c"),
 		},
 		{
-			name: "removes",
-			lhs:  NewString("a", "b", "c"),
-			rhs:  NewString("a"),
-			want: NewString("b", "c"),
+			name:  "removes",
+			left:  NewString("a", "b", "c"),
+			right: NewString("a"),
+			want:  NewString("b", "c"),
 		},
 	}
-	runTestCaseStringSlice(t, testCases, func(lhs, rhs String) String {
-		return lhs.Difference(rhs)
+	runTestCaseStringSlice(t, testCases, func(left, right String) String {
+		return left.Difference(right)
 	})
 }
 
 func TestStringUnion(t *testing.T) {
 	testCases := []testCaseString{
 		{
-			name: "both empty",
-			lhs:  NewString(),
-			rhs:  NewString(),
-			want: NewString(),
+			name:  "both empty",
+			left:  NewString(),
+			right: NewString(),
+			want:  NewString(),
 		},
 		{
-			name: "empty rhs",
-			lhs:  NewString("a", "b", "c"),
-			rhs:  NewString(),
-			want: NewString("a", "b", "c"),
+			name:  "empty right",
+			left:  NewString("a", "b", "c"),
+			right: NewString(),
+			want:  NewString("a", "b", "c"),
 		},
 		{
-			name: "empty lhs",
-			lhs:  NewString(),
-			rhs:  NewString("a", "b", "c"),
-			want: NewString("a", "b", "c"),
+			name:  "empty left",
+			left:  NewString(),
+			right: NewString("a", "b", "c"),
+			want:  NewString("a", "b", "c"),
 		},
 		{
-			name: "ignores collisions",
-			lhs:  NewString("a", "b", "c"),
-			rhs:  NewString("a", "b", "c"),
-			want: NewString("a", "b", "c"),
+			name:  "ignores collisions",
+			left:  NewString("a", "b", "c"),
+			right: NewString("a", "b", "c"),
+			want:  NewString("a", "b", "c"),
 		},
 		{
-			name: "unions",
-			lhs:  NewString("a", "b"),
-			rhs:  NewString("c", "d"),
-			want: NewString("a", "b", "c", "d"),
+			name:  "unions",
+			left:  NewString("a", "b"),
+			right: NewString("c", "d"),
+			want:  NewString("a", "b", "c", "d"),
 		},
 	}
-	runTestCaseStringSlice(t, testCases, func(lhs, rhs String) String {
-		return lhs.Union(rhs)
+	runTestCaseStringSlice(t, testCases, func(left, right String) String {
+		return left.Union(right)
 	})
 }
 
 func TestStringIntersect(t *testing.T) {
 	testCases := []testCaseString{
 		{
-			name: "both empty",
-			lhs:  NewString(),
-			rhs:  NewString(),
-			want: NewString(),
+			name:  "both empty",
+			left:  NewString(),
+			right: NewString(),
+			want:  NewString(),
 		},
 		{
-			name: "empty rhs",
-			lhs:  NewString("a", "b", "c"),
-			rhs:  NewString(),
-			want: NewString(),
+			name:  "empty right",
+			left:  NewString("a", "b", "c"),
+			right: NewString(),
+			want:  NewString(),
 		},
 		{
-			name: "empty lhs",
-			lhs:  NewString(),
-			rhs:  NewString("a", "b", "c"),
-			want: NewString(),
+			name:  "empty left",
+			left:  NewString(),
+			right: NewString("a", "b", "c"),
+			want:  NewString(),
 		},
 		{
-			name: "all",
-			lhs:  NewString("a", "b", "c"),
-			rhs:  NewString("a", "b", "c"),
-			want: NewString("a", "b", "c"),
+			name:  "all",
+			left:  NewString("a", "b", "c"),
+			right: NewString("a", "b", "c"),
+			want:  NewString("a", "b", "c"),
 		},
 		{
-			name: "intersect",
-			lhs:  NewString("a", "b"),
-			rhs:  NewString("b", "c"),
-			want: NewString("b"),
+			name:  "intersect",
+			left:  NewString("a", "b"),
+			right: NewString("b", "c"),
+			want:  NewString("b"),
 		},
 	}
-	runTestCaseStringSlice(t, testCases, func(lhs, rhs String) String {
-		return lhs.Intersect(rhs)
+	runTestCaseStringSlice(t, testCases, func(left, right String) String {
+		return left.Intersect(right)
 	})
 }
 
-func runTestCaseStringSlice(t *testing.T, testCases []testCaseString, f func(lhs, rhs String) String) {
+func runTestCaseStringSlice(t *testing.T, testCases []testCaseString, f func(left, right String) String) {
 	for _, tc := range testCases {
 		runTestCaseString(t, tc, f)
 	}
 }
 
-func runTestCaseString(t *testing.T, tc testCaseString, f func(lhs, rhs String) String) {
+func runTestCaseString(t *testing.T, tc testCaseString, f func(left, right String) String) {
 	t.Run(tc.name, func(t *testing.T) {
-		lhsLen := len(tc.lhs)
-		rhsLen := len(tc.rhs)
+		leftLen := len(tc.left)
+		rightLen := len(tc.right)
 
-		got := f(tc.lhs, tc.rhs)
+		got := f(tc.left, tc.right)
 		assert.Equal(t, tc.want, got)
 
-		assert.Len(t, tc.lhs, lhsLen, "LHS was changed")
-		assert.Len(t, tc.rhs, rhsLen, "RHS was changed")
+		assert.Len(t, tc.left, leftLen, "LEFT was changed")
+		assert.Len(t, tc.right, rightLen, "RIGHT was changed")
 	})
 }
