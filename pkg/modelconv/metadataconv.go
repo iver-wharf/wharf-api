4831f03e6650c42cbf11baf8c0349adf3cb88665
Kalle Jillheden <kalle.jillheden@iver.se>
2021-11-11 12:25:18 +0100

    Fixed linting

--- pkg/modelconv/metadataconv.go
+++ pkg/modelconv/metadataconv.go
@@ -5,6 +5,8 @@ import (
 	"github.com/iver-wharf/wharf-api/pkg/model/response"
 )
 
+// DBTimeMetadataToResponse converts a database timestamp metadata to a response
+// timestamp metadata.
 func DBTimeMetadataToResponse(timeMetadata database.TimeMetadata) response.TimeMetadata {
 	return response.TimeMetadata{
 		CreatedAt: timeMetadata.CreatedAt,
