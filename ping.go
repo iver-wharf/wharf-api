a31a0e3bd8106dbf12323d3269e4cce68f5512f2
Kalle Fagerberg <kalle.fagerberg@iver.se>
2021-11-18 14:54:19 +0100

    ping.go

--- ping.go
+++ ping.go
@@ -17,15 +17,15 @@ func (m healthModule) Register(g *gin.RouterGroup) {
 // Deprecated: Not part of the /api group for endpoints. Tentatively planned
 // for complete removal in v6.
 func (m healthModule) DeprecatedRegister(e *gin.Engine) {
-	e.GET("/", m.pingHandler)
-	e.GET("/health", m.healthHandler)
+	e.GET("/", m.pingHandler)         // added in v0.1.8
+	e.GET("/health", m.healthHandler) // added in v0.7.1
 }
 
 // pingHandler godoc
 // @id pingHandler
 // @summary Ping
 // @description You guessed it. Pong.
-// @description Added in TODO.
+// @description Added in v4.2.0.
 // @tags health
 // @produce json
 // @success 200 {object} response.Ping
@@ -38,7 +38,7 @@ func (m healthModule) pingHandler(c *gin.Context) {
 // @id getHealth
 // @summary Healthcheck for the API
 // @description To be used by Kubernetes or alike.
-// @description Added in TODO.
+// @description Added in v0.7.1.
 // @tags health
 // @produce json
 // @success 200 {object} response.HealthStatus
