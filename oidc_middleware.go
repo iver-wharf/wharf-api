179fde82a4bb75b2d730496140c757d69451c279
Fredrik <fdyrvold@outlook.com>
2021-11-24 12:02:05 +0100

    Update copyrights text
    Co-authored-by: kalle (jag) <kalle.jillheden@iver.se>

--- oidc_middleware.go
+++ oidc_middleware.go
@@ -14,7 +14,24 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
-// Largely taken from https://developer.okta.com/blog/2021/01/04/offline-jwt-validation-with-go
+// This is a modified version of the code provided in the follow blog post and
+// GitHub repository:
+// - https://developer.okta.com/blog/2021/01/04/offline-jwt-validation-with-go
+// - https://github.com/oktadev/okta-offline-jwt-validation-example/tree/a61cc73bf893686c1efe67ce86448047205826bc
+//
+// Copyright 2019 Okta, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // GetOidcPublicKeys return the public keys of the currently set WHARF_HTTP_OIDC_KeysURL.
 func GetOidcPublicKeys(config OIDCConfig) *map[string]*rsa.PublicKey {
