3954faee9280ab585a2bb2fcce939815cc941ea7
Kalle Fagerberg <kalle.fagerberg@iver.se>
2022-03-30 13:07:52 +0200

    Removed unused contains func

--- utils.go
+++ utils.go
@@ -4,15 +4,6 @@ import (
 	"github.com/iver-wharf/wharf-api/v5/pkg/model/database"
 )
 
-func contains(items []string, value string) bool {
-	for _, item := range items {
-		if item == value {
-			return true
-		}
-	}
-	return false
-}
-
 func findDefaultBranch(branches []database.Branch) (database.Branch, bool) {
 	for _, branch := range branches {
 		if branch.Default {
